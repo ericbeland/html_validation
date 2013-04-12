435b5f261bfdfe758746fc3027d9b9507f7b93b1
Eric Beland <ebeland@gmail.com>
2013-04-11 23:16:48 -0400

    Finished name conversion to html_validation

--- spec/html_validation_matcher_spec.rb
+++ spec/html_validation_matcher_spec.rb
@@ -3,7 +3,8 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 require 'html_validation/have_valid_html'
 
 describe "HTMLValidationRSpecMatcher" do
-    
+  include HTMLValidationHelpers
+
   before(:each) do
      @page = double("page")
   end
@@ -14,34 +15,26 @@ describe "HTMLValidationRSpecMatcher" do
     @page.stub :current_url => 'http://www.fake.com/good_page'
     @page.should have_valid_html
   end
-  
+
    it "should check page object with the matcher for valid HTML and fail bad HTML" do
     @page.stub :html => bad_html
     @page.stub :body => bad_html
     @page.stub :current_url => 'http://www.fake.com/bad_page'
     @page.should_not have_valid_html
   end
-  
-  
+
+
   private
 
-  def bad_html
-    '<html><title>the title<title></head><body><p>blah blah</body></html>'
-  end
-  
-  def good_html
-    '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><html><title>the title</title></head><body><p>a paragraph</p></body></html>'
-  end
-    
   def tmp_path
     is_windows = (RbConfig::CONFIG['host_os'] =~ /mswin|mingw|cygwin/)
     is_windows ? 'c:\temp\validation' : '/tmp/validation'
   end
-  
+
   # clean our temp dir without killing it
   def clean_dir(dir)
     Dir.chdir(dir)
     Dir.glob('*').each {|f| FileUtils.rm f }
   end
-  
+
 end
