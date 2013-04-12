f37868a319a6ce024956392e4bc7c784bdfdb589
Eric Beland <ebeland@gmail.com>
2013-04-12 12:17:35 -0400

    fix bad require

--- lib/html_validation.rb
+++ lib/html_validation.rb
@@ -1,4 +1,3 @@
 require 'rbconfig'
 require File.expand_path(File.join(File.dirname(__FILE__),  'html_validation/page_validations'))
 require File.expand_path(File.join(File.dirname(__FILE__),  'html_validation/html_validation_result'))
-require File.expand_path(File.join(File.dirname(__FILE__),  'html_validation/html_validation_matcher'))
