01684ad130772857d0c7ca94cc814405007d1b56
Eric Beland <ebeland@gmail.com>
2013-04-11 23:15:46 -0400

    Moved helper functions for test into helpers folder

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,8 +7,30 @@ include PageValidations
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+Dir["#{File.dirname(__FILE__)}/helpers/**/*.rb"].each {|f| require f }
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f }
 
 RSpec.configure do |config|
-  
+
+
 end
+
+  def bad_html
+    '<html><title>the title<title></head><body><p>blah blah</body></html>'
+  end
+
+  def good_html
+    html_5_doctype + '<html><title>the title</title></head><body><p>a paragraph</p></body></html>'
+  end
+
+  def dtd
+    '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">'
+  end
+
+  def html_5_doctype
+    '<!DOCTYPE html>'
+  end
+
+  def warning_html
+    html_5_doctype + '<html><title proprietary="1">h</title></head><body><p>a para</p></body></html>'
+  end
