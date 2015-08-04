37c3d287306bcdb7ee78374864b8572385f6962c
Eric Beland <ebeland@constantcontact.com>
2015-08-04 10:35:16 -0400

    Fix missing require

--- lib/html_validation.rb
+++ lib/html_validation.rb
@@ -1,3 +1,4 @@
 require 'rbconfig'
+require 'fileutils'
 require File.expand_path(File.join(File.dirname(__FILE__),  'html_validation/page_validations'))
 require File.expand_path(File.join(File.dirname(__FILE__),  'html_validation/html_validation_result'))
